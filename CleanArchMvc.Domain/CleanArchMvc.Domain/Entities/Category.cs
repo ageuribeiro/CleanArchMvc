e5448e61ad0eea4b1a0f6b463b8e7552c78a0244
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2023-10-11 16:46:10 -0300

    Foi implementado  as Interfaces  ICategory e IProduct, também foi incluído a Class Entity para poliformizar o campo Id nas entidades Category e Product

--- CleanArchMvc.Domain/CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,36 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public  class Category
+    public sealed class Category : Entity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Product> Products { get; set;}
+        public string? Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidationDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidationDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidationDomain(name);
+        }
+
+        public ICollection<Product>? Products { get; set;}
+
+        private void ValidationDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name.too short, minimum 3 characters");
+
+            Name = name;
+        }
     }
 }
--- CleanArchMvc.Domain/CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/CleanArchMvc.Domain/Entities/Product.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -7,17 +9,59 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Product
+    public sealed class Product : Entity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Price { get; set; }
-        public string Stock { get; set; }
-        public string Image { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string? Image { get; private set; }
 
-        public int CategoryId {  get; set; }
-        public Category Category { get; set; }
+
+        public Product(string name, string description, decimal price, int stock, string image, int categoryId, Category? category)
+        {
+            ValidationDomain(name, description, price, stock, image);
+        }
+
+        public Product(int id, string name, string description, decimal price, int stock, string image, int categoryId, Category? category)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidationDomain(name, description, price, stock, image);
+        }
+
+
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidationDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
+        }
+
+        private void ValidationDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid nam,. Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid nam,. Description is required");
+
+            DomainExceptionValidation.When(description.Length < 5, "Invalid description. too short, minimum 5 characters");
+
+            DomainExceptionValidation.When(price < 0, "Invalid price value");
+
+            DomainExceptionValidation.When(image.Length > 250, "Invalid image name, too long, maximim 250 characteres");
+
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+
+        }
+
+        public int CategoryId {  get;  set; }
+        public Category? Category { get;  set; }
 
     }
 }
