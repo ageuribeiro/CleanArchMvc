76eb12a152ae54a4e49f9dcce1d52e1346c34960
Ageu Lima Ribeiro <59054298+ageuribeiro@users.noreply.github.com>
2023-10-13 11:57:19 -0300

    Include new clas for text with XUnit framework for Category and Product

--- CleanArchMvc.Domain/CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/CleanArchMvc.Domain/Entities/Category.cs
@@ -35,7 +35,7 @@ namespace CleanArchMvc.Domain.Entities
         {
             DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
 
-            DomainExceptionValidation.When(name.Length < 3, "Invalid name.too short, minimum 3 characters");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
 
             Name = name;
         }
--- CleanArchMvc.Domain/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
+++ CleanArchMvc.Domain/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Entities;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,14 @@ namespace CleanArchMvc.Domain.Interfaces
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+
+        Task<Product> GetProductCategoryAsync(int? id);
+
+
+        Task<Product> Create(Product product);
+        Task<Product> Update(Product product);
+        Task<Product> Delete(Product product);
     }
 }
